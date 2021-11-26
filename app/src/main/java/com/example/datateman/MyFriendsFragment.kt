978ada109db729acd667b4059f513cd2f81b7909
denisalinda <denisalinda16@gmail.com>
2021-11-26 20:00:08 +0700

    Recycler View
    Menambahkan list data teman pada aplikasi sederhana data teman

--- app/src/main/java/com/example/datateman/MyFriendsFragment.kt
+++ app/src/main/java/com/example/datateman/MyFriendsFragment.kt
@@ -6,8 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_my_friends.*
 
 class MyFriendsFragment : Fragment() {
+    lateinit var listTeman: ArrayList<MyFriend>
+
+    private fun simulasiDataTeman() {
+        listTeman= ArrayList()
+        listTeman.add(MyFriend("Denisa Mei Linda Sari", "Perempuan", "denisalinda16@gmail.com", "085888043162", "Blitar"))
+        listTeman.add(MyFriend("Nanda Setyo Hermawan", "Laki-laki", "nandasetyo@gmail.com", "082257194910", "Malang"))
+        listTeman.add(MyFriend("Ahmad Faisal", "Laki-laki", "ahmadfaisal@gmail.com", "085336334687", "Madura"))
+        listTeman.add(MyFriend("Indah Kurniani", "Perempuan", "indah@gmail.com", "082232829301", "Malang"))
+        listTeman.add(MyFriend("Faisol Lutfi", "Laki-laki", "faisol@gmail.com", "087751957523", "Kediri"))
+        listTeman.add(MyFriend("Feri Efendi", "Laki-laki", "feri@gmail.com", "083857906912", "Madura"))
+        listTeman.add(MyFriend("Syamsul Arifin", "Laki-laki", "syamsul@gmail.com", "081914722463", "Madura"))
+        listTeman.add(MyFriend("Ahmat Baihaqi", "Laki-laki", "baihaqi@gmail.com", "082335140765", "Madura"))
+    }
+
+    private  fun tampilTeman() {
+        rv_listMyFriends.layoutManager=LinearLayoutManager(activity)
+        rv_listMyFriends.adapter=MyFriendAdapter(activity!!, listTeman)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,5 +44,16 @@ class MyFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        iniView()
+    }
+
+    private fun iniView() {
+        simulasiDataTeman()
+        tampilTeman()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
     }
 }
